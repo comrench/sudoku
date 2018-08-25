73bc1ae6f4cf584a921286216b2ece2b6e850ead
Comrench <Comrench@gmail.com>
2018-08-25 12:08:15 -0400

    grid in place

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
+import './index.css';
 
 class Square extends React.Component {
     render() {
         return (
-            <Col>
-                <Button>0</Button>
+            <Col sm={12} style={{marginLeft: 2,paddingLeft:2, marginRight: 0, paddingRight: 0,marginBottom: 1, paddingBottom: 1}}>
+                <Button bsStyle="info" bsSize="large" block>0</Button>
             </Col>
         );
     }
@@ -19,23 +20,23 @@ class Block extends React.Component {
     }
     render() {
         return (
-            <div>
-                <Col>
+            <Row style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
+                <Col sm={4} style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </Col>
-                <Col>
+                <Col sm={4} style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </Col>
-                <Col>
+                <Col sm={4} style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </Col>
-            </div>
+            </Row>
         );
     }
 }
@@ -47,22 +48,22 @@ class Board extends React.Component {
     render() {
         return (
             <Grid fluid>
-                <Row>
-                    <Col sm={4}>
+                <Row style={{marginLeft: 5,paddingLeft:5, marginRight: 0, paddingRight: 0}}>
+                    <Col style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                         {this.renderBlock(0)}
                         {this.renderBlock(1)}
                         {this.renderBlock(2)}
                     </Col>
                     {/* </Row> */}
                     {/* <Row> */}
-                    <Col sm={4}>
+                    <Col style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                         {this.renderBlock(3)}
                         {this.renderBlock(4)}
                         {this.renderBlock(5)}
                     </Col>
-                    {/* </Row> */}
-                    {/* <Row> */}
-                    <Col sm={4}>
+                    {/* </Row>
+                    <Row> */}
+                    <Col style={{marginLeft: 0,paddingLeft:0, marginRight: 0, paddingRight: 0}}>
                         {this.renderBlock(6)}
                         {this.renderBlock(7)}
                         {this.renderBlock(8)}
